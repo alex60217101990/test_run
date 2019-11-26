a537d393bb6c4336a39139f1773d34c51096093c
alex60217101990 <alex6021710@gmail.com>
2019-11-26 16:57:56 +0200

    last

--- main.go
+++ main.go
@@ -1,14 +1,13 @@
 package main
 
 import (
-	"log"
 	"fmt"
 )
 
 func main() {
 	fmt.Println("HELLO WORLD!!!")
 	// ---
-	b := 0
-	a := 111 / b
-	log.Println(a)
+	// b := 0
+	// a := 111 / b
+	// log.Println(a)
 }
