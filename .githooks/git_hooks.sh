c2c90eb9806fcf90c8bd3bdffb51300daf508fa6
alex60217101990 <alex6021710@gmail.com>
2019-11-27 13:37:32 +0200

    last

--- .githooks/git_hooks.sh
+++ .githooks/git_hooks.sh
@@ -19,6 +19,34 @@ last_commit=$(git rev-parse HEAD 2>&1)
 all_changes_files_in_commit=$(git show --name-only --oneline -- '*.go' $last_commit)
 local_branch="$(git rev-parse --abbrev-ref HEAD)"
 
+username="$(git config --list --local | grep user.email | cut -d'=' -f2 | cut -d'@' -f1)"
+valid_user_branch_regex="(feature|bugfix|hotfix)-[a-z]{1,15}\/[A-Z]{1,5}-[0-9]{1,6}"
+valid_release_branch_regex="release-\d{1,3}.\d{1,3}.\d{1,3}"
+admins=(skreitor rterentev sambaryan vstoyanov)
+is_not_admin=true
+
+if [[ "${admins[*]}" == *"${username}"* ]];
+then
+    printf "\033[32mUser ${username} is an admin\033[0m \033[0;30m\n"
+    is_not_admin=false
+else
+    printf "\033[32mUser ${username} is NOT an admin\033[0m \033[0;30m\n"
+fi
+
+wrong_user_branch=true
+if [[ $local_branch =~ $valid_user_branch_regex ]]; then
+ $wrong_user_branch=false
+fi
+wrong_release_branch=false
+if [[ $local_branch =~ $valid_release_branch_regex ]]; then
+ $wrong_release_branch=true
+fi
+if $is_not_admin && ($wrong_user_branch || $wrong_release_branch); then
+    printf "\n\t\033[41mThere is something wrong with your branch name. Branch names in this project must adhere to this contract:\033[0m\n"
+    printf "\n\t\033[41m'feature(bugfix/hotfix)-skreitor/PLA-1002' or 'release-1.2.3'.\033[0m\n"
+    printf "\n\t\033[41mYour push will be rejected. You should rename your branch to a valid name and try again.\033[0m\n"
+    exit 1
+else
 if [[ $local_branch == *"master"* || $local_branch == *"developers"* ]]; then
     printf "\033[0;36mRunning pre-commit checks on your code...\033[0m\n"
     all_path=$(echo $all_changes_files_in_commit | tr -s " " "\012")
@@ -125,7 +153,7 @@ if [[ $local_branch == *"master"* || $local_branch == *"developers"* ]]; then
             fi    
         fi
     done
-else
-    printf "\t\033[41mIncorrect GIT branch\033[0m\n"
-    exit 1
+# else
+#     printf "\t\033[41mIncorrect GIT branch\033[0m\n"
+#     exit 1
 fi
